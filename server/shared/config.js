3edabfe90546928abaa329679848e2e99c36fc40
Chase Brewer <chasebrewsky@gmail.com>
2021-03-07 13:27:38 -0500

    Add redis middleware

--- server/shared/config.js
+++ server/shared/config.js
@@ -12,6 +12,10 @@ require('dotenv');
 module.exports = Object.freeze({
   mode: process.env.SLACKORD_MODE || 'dev',
   port: process.env.SLACKORD_PORT || '3000',
-  // URL of the mongodb instance. Default value matches the docker compose url.
+  // URI of the mongodb instance. Default value matches the docker compose url.
   mongodb_uri: process.env.SLACKORD_MONGODB_URI || 'mongodb://slackord:slackord@127.0.0.1/slackord',
+  // URI of the redis instance. Default value matches the docker compose url.
+  redis_uri: process.env.SLACKORD_REDIS_URI || 'redis://127.0.0.1:6379/',
+  // Secret value used for generating secrets. MUST BE CHANGED IN PRODUCTION.
+  secret: process.env.SLACKORD_SECRET || 'random-secret',
 });
