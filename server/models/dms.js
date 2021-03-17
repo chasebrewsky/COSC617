cd73921ec5ab913421e30320079d9cdae561be58
Chase Brewer <chasepbrewer@gmail.com>
2021-03-16 21:35:48 -0400

    Create basic models for endpoints (#7)
    * Create basic models for endpoints

---
+++ server/models/dms.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+module.exports = {
+  DMChannel: mongoose.model('DMChannel', new mongoose.Schema({
+    id: ObjectId,
+    users: { type: [ObjectId] }
+  })),
+  DMChannelMessage: mongoose.model('DMChannelMessage', new mongoose.Schema({
+    id: ObjectId,
+    channelId: { type: ObjectId, require: true },
+    userId: { type: ObjectId, required: true },
+    content: { type: String, required: true },
+  }))
+}
