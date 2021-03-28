028480ebddc283fe09f0af8dc2cce41a2778c8a7
Devor27 <35972683+Devor27@users.noreply.github.com>
2021-03-28 10:58:39 -0400

    Added api endpoints

--- server/routes/channel.js
+++ server/routes/channel.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const { model } = require('mongoose');
 const Channel = require('../models/channel');
+const db = require('../shared/db');
 
+//Test page for showing channels
+//TODO - make this use API
 router.get('/channels', async (req, res) => {
 
   var listString="";
@@ -17,9 +20,107 @@ router.get('/channels', async (req, res) => {
 
 });
 
+//Create Channel
+//TODO - How to test post?
+router.post('api/channels/', async (req, res) => {
+
+  var channelName=req.body.name;
+
+  console.log(channelName);
+
+  var filter = {
+    "channelName":channelName
+  };
+
+  const channelDoc = await Channel.findOneAndUpdate(filter, {}, {
+    new: true,
+    upsert: true
+  });
+
+
+  const result = {
+    "id": channelDoc._id,
+    "name": channelDoc.channelName
+  };
+
+  res.json(result);
+
+});
+
+//Update Channel Name
+//TODO - test put request
+router.put('api/channels/:id', async (req, res) => {
+
+  var channelName=req.body.name;
+  var channelId=req.params.id;
+
+  console.log(channelName);
+  console.log(channelId);
+
+  var filter = {
+    "_id": channelId
+  };
+
+  var update = {
+    "channelName":channelName
+  };
+
+  const channelDoc = await Channel.findOneAndUpdate(filter, update, {
+    new: true,
+    upsert: false
+  });
+
+
+  const result = {
+    "id": channelDoc._id,
+    "name": channelDoc.channelName
+  };
+
+  res.json(result);
+
+});
+
+//TODO - Channel Messages
+
+//Return list of all channels
+router.get('/api/channels', async (req, res) => {
+
+  const channelList = await Channel.find({});
+
+  res.json(channelList);
+
+});
+
+//Return channel with matching id
+router.get('/api/channels/:id', async (req, res) => {
+
+  var filter = {
+    "_id":req.params.id
+  }
+
+  var channelQuery = await Channel.findOne(filter).exec();
+
+  var channelResponse = {
+    "id":channelQuery._id,
+    "name":channelQuery.channelName
+  };
+
+  res.json(channelResponse);
+
+});
+
+//For testing only
 router.get('/channels/mongoSetup', async (req, res) => {
   
-  //Channel.collection.drop();
+  // db.dropCollection("channels", function(err, result) {
+  //   if(err) {
+  //     console.log(err);
+  //   }
+  //   else 
+  //   { 
+  //     console.log("Collection Channels dropped.")
+  //   }
+  // })
 
   const channelArray=["a","b","c","d","e","f","g","h","i"];
 
@@ -31,7 +132,7 @@ router.get('/channels/mongoSetup', async (req, res) => {
     });
   }
 
-  Channel.find({
+  await Channel.find({
     "channelName": { $in: channelArray }
   }, function(err, docs) {
       console.log(docs)
